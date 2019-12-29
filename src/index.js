a0a9cc93a591abbe2fdbc24cf813bdb5bdeb6d76
gebhartn <nicholas.gebhart@gmail.com>
2019-12-28 17:18:47 -0800

    feat: adds feed resolver
    display a list of Link types

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
+
+const links = [
+  {
+    id: 'link-0',
+    url: 'howtographql.com',
+    description: 'stack tutorial',
+  },
+];
 
 const resolvers = {
   Query: {
     info: () => 'Api is listening',
+    feed: () => links,
   },
 };
 
