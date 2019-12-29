274d6f620dc24ce99676ebbbc8e137f1e08c0fc6
gebhartn <nicholas.gebhart@gmail.com>
2019-12-28 21:11:47 -0800

    feat: feed pagination, filter
    adds filter conditions to feed resolver
    adds pagination conditions to feed resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,18 @@
-function feed(parent, args, context) {
-  return context.prisma.links();
+async function feed(parent, args, context) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {};
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+  });
+  return links;
 }
 
 module.exports = {
--- src/schema.js
+++ src/schema.js
@@ -3,13 +3,20 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     info: String!
-    feed: [Link!]!
+    feed(filter: String, skip: Int, first: Int): [Link!]!
   }
 
   type Mutation {
     post(url: String!, description: String!): Link!
     signup(email: String!, password: String!, name: String!): AuthPayload
     login(email: String!, password: String!): AuthPayload
+    vote(linkId: ID!): Vote
+  }
+
+  type Vote {
+    id: ID!
+    link: Link!
+    user: User!
   }
 
   type AuthPayload {
@@ -28,7 +35,13 @@ const typeDefs = gql`
     id: ID!
     description: String!
     url: String!
-    postedBy: String!
+    postedBy: User!
+    votes: [Vote!]!
+  }
+
+  type Subscription {
+    newLink: Link
+    newVote: Vote
   }
 `;
 
